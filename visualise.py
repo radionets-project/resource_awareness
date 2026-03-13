a9dd6cdf886c1d4590beb918aa06399248f0785b
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-03-13 23:56:07 +0100

    chore: update formatting of visualise.py

--- visualise.py
+++ visualise.py
@@ -1,11 +1,15 @@
-from strep.index_scale import load_database, scale_and_rate
 from strep.elex.app import Visualization
+from strep.index_scale import load_database, scale_and_rate
 
 fname = "./results.csv"
+
 # load database and meta information (if available)
 database, meta = load_database(fname)
+
 # index-scale and rate database
 rated_database = scale_and_rate(database, meta)
+
 # start the interactive exploration tool
 app = Visualization(rated_database)
 app.run(port=8899)
+
